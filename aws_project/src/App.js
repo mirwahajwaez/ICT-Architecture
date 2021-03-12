13f21030bc3b8153baba4ae459f6efd39e63d4bc
MohamedAz22 <66697663+MohamedAz22@users.noreply.github.com>
2021-03-12 22:40:06 +0100

    Add files via upload

---
+++ aws_project/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import UploadFiles from "./components/upload-files.component";
+
+function App() {
+  return (
+    <div className="container" style={{ width: "600px" }}>
+      <div className="my-2">
+        <h4>AWS Download bestanden</h4>
+      </div>
+
+      <UploadFiles />
+    </div>
+  );
+}
+
+export default App;
